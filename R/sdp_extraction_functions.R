ee2316dcb2df634c4de2591fd94e6fa621db6472
Ian Breckheimer <ikb@rmbl.org>
2023-01-12 15:01:48 -0700

    Updated documentation for time-series functionality.

--- R/sdp_extraction_functions.R
+++ R/sdp_extraction_functions.R
@@ -2,6 +2,7 @@
 #'
 #' @param catalog_id A valid catalog number for an SDP dataset. This is in the `CatalogID` field for information returned by `sdp_get_catalog()`.
 #' @param url A valid URL (e.g. https://path.to.dataset.tif) for the cloud-based dataset.
+#' @param years For time-series data, a numeric vector specifying which years to return. The default `NULL` returns all available years.
 #' @param ... Other arguments to pass to the `terra::rast()` function.
 #'
 #' @details Files headers are read from cloud-based datasets using the `terra` package, but the full dataset is not downloaded locally. Instead `terra` uses the web-based file system embedded in GDAL (VSICURL) to access datasets on the cloud. For large datasets and slow network connections, the function might take up to a minute to complete.
@@ -42,8 +43,8 @@ sdp_get_raster <- function(catalog_id=NULL,url=NULL,years=NULL,...){
       }else if((length(years_cat) < length(years)) & length(years_cat) > 0){
         warning(paste("No dataset available for some specified years. \n Returning data for",years_cat))
       }
-
       raster_path <- unlist(lapply(years_cat, FUN=function(x) {gsub("{year}",x,raster_path,fixed=TRUE)}))
+      print(paste("Returning dataset with",length(years_cat),"layers be patient..."))
       raster <- terra::rast(raster_path,...)
       names(raster) <- years_cat
       return(raster)
@@ -51,6 +52,7 @@ sdp_get_raster <- function(catalog_id=NULL,url=NULL,years=NULL,...){
     }else if(is.null(years) & cat_line$TimeSeriesType=="Yearly"){
       cat_years <- cat_line$MinYear:cat_line$MaxYear
       raster_path <- unlist(lapply(cat_years, FUN=function(x) {gsub("{year}",x,raster_path,fixed=TRUE)}))
+      print(paste("Returning dataset with",length(cat_years),"layers, be patient..."))
       raster <- terra::rast(raster_path,...)
       names(raster) <- cat_years
       return(raster)
